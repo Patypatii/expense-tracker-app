5d7e637acddd21e9dad7c4ec20fcf5db04a3bd04
patyupatii <patrickwambugu707@gmail.com>
2025-06-09 07:31:56 +0300

    complted the Ui authentication

--- mobile/app/(auth)/sign-up.jsx
+++ mobile/app/(auth)/sign-up.jsx
@@ -1,16 +1,23 @@
-import * as React from 'react'
+
 import { Text, TextInput, TouchableOpacity, View } from 'react-native'
 import { useSignUp } from '@clerk/clerk-expo'
 import { Link, useRouter } from 'expo-router'
+import { useState } from 'react'
+import { styles } from "@/assets/styles/auth.styles.js"
+import Ionicons from '@expo/vector-icons'
+import { COLORS } from "../../constants/colors.js"
+import { Image } from 'expo-image'
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view'
 
 export default function SignUpScreen() {
     const { isLoaded, signUp, setActive } = useSignUp()
     const router = useRouter()
 
-    const [emailAddress, setEmailAddress] = React.useState('')
-    const [password, setPassword] = React.useState('')
-    const [pendingVerification, setPendingVerification] = React.useState(false)
-    const [code, setCode] = React.useState('')
+    const [emailAddress, setEmailAddress] = useState('')
+    const [password, setPassword] = useState('')
+    const [pendingVerification, setPendingVerification] = useState(false)
+    const [code, setCode] = useState('')
+    const [error, setError] = useState("")
 
     // Handle submission of sign-up form
     const onSignUpPress = async () => {
@@ -57,54 +64,96 @@ export default function SignUpScreen() {
                 console.error(JSON.stringify(signUpAttempt, null, 2))
             }
         } catch (err) {
-            // See https://clerk.com/docs/custom-flows/error-handling
-            // for more info on error handling
-            console.error(JSON.stringify(err, null, 2))
+            if (err.errors?.[0]?.code === 'form_identifier_exists') {
+                // Handle error if user already exists
+                setError("User already exists. Please sign in or use a different email.")
+            } else {
+                setError("An error occurred. Please try again.")
+            }
+            console.log(err);
+
         }
     }
 
     if (pendingVerification) {
         return (
-            <>
-                <Text>Verify your email</Text>
+            <View style={styles.verificationContainer}>
+                <Text style={styles.verificationTitle}>Verify your email</Text>
+
+                {
+                    error ? (
+                        <View style={styles.errorBox}>
+                            <Ionicons name="alert-circle" size={20} color={COLORS.expense} />
+                            <Text style={styles.errorText}>{error}</Text>
+                            <TouchableOpacity onPress={() => setError("")}>
+                                <Ionicons name="close" size={20} color={COLORS.textLight} />
+                            </ TouchableOpacity>
+                        </View>
+                    ) : null
+                }
                 <TextInput
+                    style={[styles.verificationInput, error && styles.errorInput]}
                     value={code}
                     placeholder="Enter your verification code"
+                    placeholderTextColor="#9A8478"
                     onChangeText={(code) => setCode(code)}
                 />
-                <TouchableOpacity onPress={onVerifyPress}>
-                    <Text>Verify</Text>
+                <TouchableOpacity onPress={onVerifyPress} style={styles.button}>
+                    <Text style={styles.buttonText}>Verify</Text>
                 </TouchableOpacity>
-            </>
+            </View>
         )
     }
 
     return (
-        <View>
-            <>
-                <Text>Sign up</Text>
+        <KeyboardAwareScrollView
+            style={{ flex: 1 }}
+            contentContainerStyle={{ flexGrow: 1 }}
+            enableOnAndroid={true}
+            enableAutomaticScroll={true}
+            extraScrollHeight={100}
+
+        >
+            <View style={styles.container}>
+                <Image source={require('../../assets/images/revenue-i2.png')} style={styles.illustration} />
+                <Text style={styles.title}>Create Account</Text>
+                {
+                    error ? (
+                        <View style={styles.errorBox}>
+                            <Ionicons name="alert-circle" size={20} color={COLORS.expense} />
+                            <Text style={styles.errorText}>{error}</Text>
+                            <TouchableOpacity onPress={() => setError("")}>
+                                <Ionicons name="close" size={20} color={COLORS.textLight} />
+                            </ TouchableOpacity>
+                        </View>
+                    ) : null
+                }
                 <TextInput
+                    style={[styles.input, error && styles.errorInput]}
                     autoCapitalize="none"
                     value={emailAddress}
                     placeholder="Enter email"
+                    placeholderTextColor="#9A8478"
                     onChangeText={(email) => setEmailAddress(email)}
                 />
                 <TextInput
+                    style={[styles.input, error && styles.errorInput]}
                     value={password}
                     placeholder="Enter password"
+                    placeholderTextColor="#9A8478"
                     secureTextEntry={true}
                     onChangeText={(password) => setPassword(password)}
                 />
-                <TouchableOpacity onPress={onSignUpPress}>
-                    <Text>Continue</Text>
+                <TouchableOpacity style={styles.button} onPress={onSignUpPress}>
+                    <Text style={styles.buttonText}>Sign Up</Text>
                 </TouchableOpacity>
-                <View style={{ display: 'flex', flexDirection: 'row', gap: 3 }}>
-                    <Text>Already have an account?</Text>
+                <View style={styles.footerContainer}>
+                    <Text style={styles.footerText}>Already have an account?</Text>
                     <Link href="/sign-in">
-                        <Text>Sign in</Text>
+                        <Text style={styles.linkText}>Sign in</Text>
                     </Link>
                 </View>
-            </>
-        </View>
+            </View>
+        </KeyboardAwareScrollView>
     )
-}
+} 
